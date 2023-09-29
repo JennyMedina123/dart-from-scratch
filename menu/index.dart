a1ee5be519aa0da91f1113698d23f76a3166bf5a
jenny medina <jennymedina315551@correo.itm.edu.co>
2023-09-28 22:25:11 -0500

    ejercicio final Dart

--- menu/index.dart
+++ menu/index.dart
@@ -1,21 +1,21 @@
 class Menu {
   void MostrarMenu() {
-    print('=== Menú ===');
-    print('1. Desafío 1: Positivo, negativo o cero');
-    print('2. Desafío 2: Operaciones básicas entre dos números');
-    print('3. Desafío 3: Cuadrado de un número');
-    print('4. Desafío 4: Área de un círculo');
-    print('5. Desafío 5: Día de la semana');
-    print('6. Desafío 6: Impuesto');
-    print('7. Desafío 7: División');
-    print('8. Desafío 8: Suma de impares entre 1 y 100');
-    print('9. Desafío 9: Suma de fracciones');
-    print('10. Desafío 10: Palabra en orden inverso');
-    print('11. Desafío 11: Promedio de tres números');
-    print('12. Desafío 12: Número más grande de cinco');
-    print('13. Desafío 13: Palíndromo');
-    print('14. Desafío 14: Par o impar');
-    print('15. Desafío 15: En rango de 0 a 10');
+    print('===== Menú =====');
+    print('1. Ejercicio 1: Solicita al usuario un número y eleva este número al cuadrado solo si es positivo.');
+    print('2. Ejercicio 2: Solicita al usuario dos números. Si el primero es mayor, devuelva su doble, de lo contrario devuelva el triple del segundo.');
+    print('3. Ejercicio 3: Pide al usuario un número. Si es positivo, devuelve su raíz cuadrada, de lo contrario, devuelve su cuadrado.');
+    print('4. Ejercicio 4: Pide al usuario el radio de un círculo y calcula su perímetro.');
+    print('5. Ejercicio 5: Solicita al usuario un número entre 1 y 7 y muestra el día de la semana correspondiente, pero solo considerando los días laborables.');
+    print('6. Ejercicio 6: Solicita al usuario su salario anual y, si este excede los 12000, muestra el impuesto a pagar que es el 15% del excedente.');
+    print('7. Ejercicio 7: Solicita dos números y muestra el residuo de la división del primero entre el segundo.');
+    print('8. Ejercicio 8: Calcula y muestra la suma de los números pares entre 1 y 50.');
+    print('9. Ejercicio 9: Solicita al usuario los valores para dos fracciones y muestra la diferencia entre esas fracciones.');
+    print('10. Ejercicio 10: Pide una palabra al usuario y muestra la longitud de esa palabra.');
+    print('11. Ejercicio 11: Pide al usuario cuatro números y muestra el promedio.');
+    print('12. Ejercicio 12: Pide al usuario cinco números y muestra el más pequeño.');
+    print('13. Ejercicio 13: Pide una palabra al usuario y devuelve el número de vocales en esa palabra.');
+    print('14. Ejercicio 14: Pide un número al usuario y devuelve el factorial de ese número.');
+    print('15. Ejercicio 15: Pide un numero al usuario y verifica si esta en el rango de 10 a 20 (ambos incluidos).');
     print('0. q  o tambien la palabra exit');
   }
 }
